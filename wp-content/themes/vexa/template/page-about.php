74abe861add5d6c2d596dc50cd503b9276448c7e
tuan2099 <gwoqkepspsld@gmail.com>
2025-10-02 23:18:29 +0700

    update code

---
+++ wp-content/themes/vexa/template/page-about.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Template name: About
+ * @author : Hoang Anh Tuan
+ */
+
+get_header();
+?>
+
+<!-- About Section -->
+<?php get_template_part('template-parts/about-section'); ?>
+
+<!-- Ourteam Section -->
+<?php get_template_part('template-parts/ourteam-section'); ?>
+
+<!-- Testerminal Section -->
+<?php get_template_part('template-parts/testerminal-section'); ?>
+<?php
+get_footer(); ?>
